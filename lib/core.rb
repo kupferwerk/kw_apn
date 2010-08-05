e0631e6910de3a59d4a54d8e609d709f982ab9b6
Andreas Gerauer <helle@fraggaz.de>
2010-08-05 10:36:16 +0200

    removed unused classs

--- lib/core.rb
+++ lib/core.rb
@@ -75,13 +75,4 @@ module KwAPN
 
   end
 
-  class FeedbackServiceResponce
-
-    attr_accessor :timestamp, :token
-
-    def initialize bitstring
-      @timestamp, tokenlen, @token = *bitstring.unpack('NnH*')
-    end
-
-  end
 end
